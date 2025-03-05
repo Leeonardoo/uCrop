5de59b1db766675be031ca3af8df66b8c5a257ff
“masato1230” <0ht3851f11t426k@ezweb.ne.jp>
2025-03-05 17:57:02 +0900

    Apply specified statusbarcolor for android15 or newer

--- sample/src/main/java/com/yalantis/ucrop/sample/SampleActivity.java
+++ sample/src/main/java/com/yalantis/ucrop/sample/SampleActivity.java
@@ -6,6 +6,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.Animatable;
 import android.graphics.drawable.Drawable;
@@ -481,6 +482,7 @@ public class SampleActivity extends BaseActivity implements UCropFragmentCallbac
     private UCrop advancedConfig(@NonNull UCrop uCrop) {
         UCrop.Options options = new UCrop.Options();
 
+        options.setStatusBarColor(Color.WHITE);
         int checkedCompressionRadioButtonId =
                 mRadioGroupCompressionSettings.getCheckedRadioButtonId();
         if (checkedCompressionRadioButtonId == R.id.radio_png) {
